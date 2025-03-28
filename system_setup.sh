8ec8144dbf8c03d9077a29a580fb01e646a42367
Trupti Desai <tdesai0896@gmail.com>
2025-03-28 15:31:51 +0530

    Create system_setup.sh

---
+++ system_setup.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install openjdk-17-jdk -y
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
